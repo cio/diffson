13b605f69a89d18812554160fe5ebfc77825964a
Christian Cikryt <christian.cikryt@here.com>
2016-05-20 17:04:35 +0200

    Fix: Add a slash between path elements when printing

--- src/main/scala/gnieh/diffson/pointer.scala
+++ src/main/scala/gnieh/diffson/pointer.scala
@@ -69,6 +69,6 @@ private final case class Path(prefix: Pointer, elem: String) extends Pointer {
   override def toString =
     prefix match {
       case Root => f"/${elem.replace("~", "~0").replace("/", "~1")}"
-      case _    => f"${prefix}${elem.replace("~", "~0").replace("/", "~1")}"
+      case _    => f"${prefix}/${elem.replace("~", "~0").replace("/", "~1")}"
     }
 }
